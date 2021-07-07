1b7817cc41ada8f4aafc8b919ef225e1526646fc
avaas7 <avashpalikhea@gmail.com>
2021-07-07 23:22:39 +0545

    My health 5 commit

--- app/src/main/java/com/example/myhealthapplication/MapsActivity.java
+++ app/src/main/java/com/example/myhealthapplication/MapsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.content.DialogInterface;
@@ -14,17 +15,27 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapsActivity extends AppCompatActivity {
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-
-
+        SupportMapFragment supportMapFragment4 = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragMap);
+        supportMapFragment4.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
 }
